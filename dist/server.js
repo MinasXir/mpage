0a979fa2bedcdca1fa58e5e7b795238cd119601c
MinasXirakias <minasxirak@gmail.com>
2021-01-05 19:17:21 +0200

    Initial commit

---
+++ dist/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+
+// server your css as static
+app.use(express.static(__dirname + '/public'))
+
+console.log("Server Started");
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+})
+app.get("/about", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+})
+app.get("/contact", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
+})
+
+// start your server
+app.listen(3000, () => console.log('Server started at port 3000'))
